aa5c3e42d3d41634eebc465d37dba57b90b1ea03
Walter Fan (yafan) <yafan@cisco.com>
2019-11-09 23:03:37 +0800

    add helloprotocol example

---
+++ helloprotocol/src/main/java/com/github/walterfan/helloprotocol/WsServer.java
@@ -0,0 +1,38 @@
+package com.github.walterfan.helloprotocol;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+
+public class WsServer {
+    private static final int PORT = 9000;
+
+    public static void main(String[] args) {
+        // Configure the server.
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HTTPInitializer());
+
+            Channel ch = b.bind(PORT).sync().channel();
+
+            ch.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
