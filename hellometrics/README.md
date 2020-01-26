594b0ba0f360b814a3cd7089ea399bfb109f7b73
Walter Fan (yafan) <yafan@cisco.com>
2020-01-27 00:41:57 +0800

    update example

--- hellometrics/README.md
+++ hellometrics/README.md
@@ -1,4 +1,4 @@
 mvn clean compile assembly:single
-
+chmod 600 jmxremote.password
 run_oom_demo.sh
 
--- hellometrics/run_oom_demo.sh
+++ hellometrics/run_oom_demo.sh
@@ -1,13 +1,27 @@
 HOME_PATH=.
 CLASS_PATH=./target/hellometrics-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN_CLASS=com.github.walterfan.hellometrics.OomDemo
-startTime=$(date +%Y%m%d)
+startTime=$(date +%Y-%m-%d_%H_%M_%S)
 
-JAVA_OPTS="-server -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$HOME_PATH/heapdump.hprof -Xms128m -Xmx256m -XX:StringTableSize=100003 -Dsun.net.inetaddr.ttl=15 -Djava.security.egd=file:/dev/./urandom -Duser.timezone=GMT -Dcom.datastax.driver.FORCE_NIO=true -Dfile.encoding=UTF8  -cp $CLASS_PATH $MAIN_CLASS"
+JAVA_OPTS="-server -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$HOME_PATH/heapdump_$startTime.hprof -XX:+UseParallelGC -Xms128m -Xmx256m -XX:MaxMetaspaceSize=64m -XX:StringTableSize=100003 -Dsun.net.inetaddr.ttl=15 -Djava.security.egd=file:/dev/./urandom -Duser.timezone=GMT -Dfile.encoding=UTF8"
 
 JMX_OPS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=8091 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.password.file=./jmxremote.password -Dcom.sun.management.jmxremote.access.file=./jmxremote.access"
 
+CMD_TYPE=multimap
+CMD_COUNT=100000000
 
-cmd="java $JAVA_OPTS $JMX_OPS >> $HOME_PATH/oom_demo.$startTime.log 2>&1 &"
+if [ $# -eq 1 ]
+  then
+    CMD_TYPE=$1
+fi
+if [ $# -eq 2 ]
+  then
+    CMD_TYPE=$1
+    CMD_COUNT=$2
+fi
 
+APP_OPTS="-t $CMD_TYPE -c $CMD_COUNT"
+
+cmd="java $JAVA_OPTS $JMX_OPS -cp $CLASS_PATH $MAIN_CLASS $APP_OPTS"
+echo $cmd
 $cmd
